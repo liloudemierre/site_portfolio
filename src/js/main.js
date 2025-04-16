b91dfb66b442444932f24acf5f16d225acd11e2b
liloudemierre <161479296+liloudemierre@users.noreply.github.com>
2025-04-16 16:13:59 +0200

    pre-rendu

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,8 @@ gsap.set(".projet-animate", { x: 400 });
 gsap.to(".projet-animate", {
   scrollTrigger: {
     trigger: ".projet-animate",
-    start: "top 99%",
-    end: "bottom 25%",
+    start: "top 85%",
+    end: "bottom 60%",
     scrub: 0.1,
     markers: true,
   },

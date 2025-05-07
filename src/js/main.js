0bd01449dc55b2d6f0ed87d145907f374b16e8d8
liloudemierre <161479296+liloudemierre@users.noreply.github.com>
2025-05-07 09:50:04 +0200

    avant responsive

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,11 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.set(".projet-animate", { x: 400 });
+gsap.set(".project-animate", { x: 400 });
 
-gsap.to(".projet-animate", {
+gsap.to(".project-animate", {
   scrollTrigger: {
-    trigger: ".projet-animate",
+    trigger: ".project-animate",
     start: "top 85%",
     end: "bottom 60%",
     scrub: 0.1,
@@ -15,4 +15,20 @@ gsap.to(".projet-animate", {
   x: 0,
   duration: 2,
   ease: "power2.out",
+});
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.top = `${e.clientY}px`;
+  cursor.style.left = `${e.clientX}px`;
+});
+
+document.addEventListener("mouseover", (e) => {
+  const target = e.target;
+  if (target.closest("a, button, .clickable")) {
+    cursor.classList.add("clickable");
+  } else {
+    cursor.classList.remove("clickable");
+  }
 });
